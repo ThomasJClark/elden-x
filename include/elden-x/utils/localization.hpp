584a7de9f86f1ee6d7b166142c0fe71620d6ac4b
Tom Clark <the.thomas.j.clark@gmail.com>
2024-11-24 18:39:59 -0500

    Remove Steamworks wrapper

--- include/elden-x/utils/localization.hpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-
-/**
- * Return the player's selected language using the Steamworks SDK
- *
- * https://partner.steamgames.com/doc/api/ISteamApps#GetCurrentGameLanguage
- */
-std::string get_steam_language();
