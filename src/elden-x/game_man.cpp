48b36ae0cfa9181cd0c247091f2a7bac14d1fd41
Tom Clark <the.thomas.j.clark@gmail.com>
2025-01-06 19:30:45 -0500

    Fix GameMan AOB

--- src/elden-x/game_man.cpp
+++ src/elden-x/game_man.cpp
@@ -10,7 +10,10 @@ from::CS::GameMan *from::CS::GameMan::instance()
     if (!address)
     {
         address = modutils::scan<from::CS::GameMan *>({
-            .aob = "48 8B 3D ?? ?? ?? ?? 44 0F B6 30 48 85 FF 75",
+            .aob = "48 8B 05 ?? ?? ?? ??" // mov rax, qword ptr [GameMan]
+                   "80 B8 ?? ?? ?? ?? 0D" // cmp byte ptr rax->???, 13
+                   "0F 94 C0"             // sete al
+                   "C3",                  // ret
             .relative_offsets = {{3, 7}},
         });
 
