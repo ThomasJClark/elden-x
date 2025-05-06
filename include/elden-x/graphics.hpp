3fa1e0d4ae94f2ce7c87f4b700823f363f0251e7
Tom Clark <the.thomas.j.clark@gmail.com>
2025-05-05 21:59:27 -0400

    Add more graphics globals

--- include/elden-x/graphics.hpp
+++ include/elden-x/graphics.hpp
@@ -15,6 +15,10 @@ class IDXGISwapChain3;
 namespace er {
 namespace GXBS {
 
+class GXDeviceContext;
+class GXProxyDeviceContext;
+class GXRenderThread;
+
 class GXCommandQueue {
 public:
     virtual ~GXCommandQueue() = 0;
@@ -48,28 +52,67 @@ public:
  */
 class globals {
 private:
-    unsigned char unk0[0x18];
-    GXCommandQueue *command_queue;
-    unsigned char unk20[0x120];
-    GXSwapChainOperatorForMultiWindow *swap_chain_operator;
-    unsigned char unk10[0xec8];
+    struct globals_impl {
+        void *unk0;
+        void *unk8;
+        int unk10;  // 0
+        GXCommandQueue *command_queue;
+        GXProxyDeviceContext *proxy_device_context;
+        unsigned char unk28;  // 0xc0
+        GXDeviceContext *device_context;
+        unsigned char unk38[0x20];
+        void *unk58;
+        void *unk60;
+        void *unk68;
+        void *unk70;
+        void *unk78;
+        void *unk80;
+        GXDeviceContext *device_context2;
+        unsigned char unk90[0xb0];
+        GXSwapChainOperatorForMultiWindow *swap_chain_operator;
+        unsigned char unk148[0xd70];
+        // GXRenderThread 6d0
+        bool resize_requested;
+        unsigned char unkeb9[0x157];
+    } impl;
+
+    static_assert(0x0 == __builtin_offsetof(globals_impl, unk0));
+    static_assert(0x8 == __builtin_offsetof(globals_impl, unk8));
+    static_assert(0x10 == __builtin_offsetof(globals_impl, unk10));
+    static_assert(0x18 == __builtin_offsetof(globals_impl, command_queue));
+    static_assert(0x28 == __builtin_offsetof(globals_impl, unk28));
+    static_assert(0x30 == __builtin_offsetof(globals_impl, device_context));
+    static_assert(0x38 == __builtin_offsetof(globals_impl, unk38));
+    static_assert(0x88 == __builtin_offsetof(globals_impl, device_context2));
+    static_assert(0x90 == __builtin_offsetof(globals_impl, unk90));
+    static_assert(0x140 == __builtin_offsetof(globals_impl, swap_chain_operator));
+    static_assert(0x148 == __builtin_offsetof(globals_impl, unk148));
+    static_assert(0xeb8 == __builtin_offsetof(globals_impl, resize_requested));
+    static_assert(0xeb9 == __builtin_offsetof(globals_impl, unkeb9));
+    static_assert(0x1010 == sizeof(globals_impl));
 
 public:
     static globals *instance();
 
     ID3D12CommandQueue *get_command_queue() {
-        if (!command_queue || !command_queue->command_queue_holder) return nullptr;
-        return command_queue->command_queue_holder->command_queue;
+        if (!impl.command_queue || !impl.command_queue->command_queue_holder) return nullptr;
+        return impl.command_queue->command_queue_holder->command_queue;
     }
 
     IDXGISwapChain3 *get_swap_chain() {
-        if (!swap_chain_operator) return nullptr;
+        if (!impl.swap_chain_operator) return nullptr;
 
-        auto &swap_chains = swap_chain_operator->swap_chains;
+        auto &swap_chains = impl.swap_chain_operator->swap_chains;
         if (swap_chains.empty()) return nullptr;
 
         return swap_chains[0].swap_chain_holder->swap_chain;
     }
+
+    /**
+     * If true, the resolution or fullscreen settings have changed. This is set to false during the
+     * DrawStep task group, and DirectX calls are made to allocate new resources.
+     */
+    bool is_resize_requested() { return impl.resize_requested; }
 };
 
 }
